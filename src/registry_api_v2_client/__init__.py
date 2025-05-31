4ffe3f3433cfc2ba792fcd57d88c178052352537
semantic-release <semantic-release>
2025-05-31 02:20:18 +0000

    1.2.0
    Automatically generated by python-semantic-release

--- src/registry_api_v2_client/__init__.py
+++ src/registry_api_v2_client/__init__.py
@@ -1,6 +1,6 @@
 """Registry API v2 Client - Async Docker Registry API v2 client with tar file utilities."""
 
-__version__ = "1.1.2"
+__version__ = "1.2.0"
 __author__ = "Kang Hee Yong"
 __email__ = "cagojeiger@naver.com"
 
